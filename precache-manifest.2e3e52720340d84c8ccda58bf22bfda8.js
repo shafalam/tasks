cb65f071ca45b2f221f0cfc899696a052d502472
shafalam <38904470+shafalam@users.noreply.github.com>
2019-12-31 20:18:41 +0200

    Updates

---
+++ precache-manifest.2e3e52720340d84c8ccda58bf22bfda8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d6616af9b6b08656dae9d7ccf4fa7e",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "a06b5da7d706bc12eaab",
+    "url": "/tasks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "851f2eab89b10b938683",
+    "url": "/tasks/static/js/2.687a360a.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/tasks/static/js/2.687a360a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a06b5da7d706bc12eaab",
+    "url": "/tasks/static/js/main.011a0b37.chunk.js"
+  },
+  {
+    "revision": "67a83c380e7b9a385b33",
+    "url": "/tasks/static/js/runtime-main.b8983021.js"
+  }
+]);
