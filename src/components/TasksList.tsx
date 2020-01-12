94e05dfd4807f5440935b797a6e8cad9666bf748
shafalam <38904470+shafalam@users.noreply.github.com>
2020-01-12 22:50:29 +0200

    added completed list

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -17,46 +17,57 @@ import AddCircleIcon from '@material-ui/icons/AddCircle';
 import MultilineTextFields from "./TextArea"
 
 const styles = (theme: Theme) =>
-({
+  ({
     root: {
       width: '100%',
-      maxWidth: '360',
+      maxWidth: '360em',
       backgroundColor: theme.palette.background.paper
     }
   });
 
-interface TaskProperty{
+interface TaskProperty {
   check: boolean;
   description: string;
-  rating: number|null;
+  rating: number | null;
   edit: boolean;
+  date?: Date;
 }
 
-interface TaskListProperty{
+interface TaskListProperty {
   classes: any
 }
 
 class TaskList extends React.Component<TaskListProperty> {
 
   aTask: TaskProperty[] = [];
+  bTask: TaskProperty[] = [];
 
   state = {
-    task: this.aTask
+    completedTask: this.aTask,
+    inCompletedTask: this.bTask
   }
 
   handleCheck = (index: number) => () => {
-    const newTasks = [...this.state.task];
+    const newTasks = [...this.state.completedTask];
+    const thatTask = newTasks[index];
     const taskCheck = newTasks[index].check;
-    newTasks[index].check = !taskCheck;
-    this.setState({task: newTasks});
+    thatTask.check = !taskCheck;
+    thatTask.date = new Date();
+    console.log("utc day " + thatTask.date.getUTCDate());
+    //newTasks[index].check = !taskCheck;
+    const inCompletedTask = [...this.state.inCompletedTask];
+
+    inCompletedTask.push(thatTask);
+    newTasks.splice(index, 1);
+    this.setState({ completedTask: newTasks, inCompletedTask: inCompletedTask });
   };
 
   handleEdit = (index: number) => {
-    const initialEditStatus = this.state.task[index].edit;
-    const newTasks = [...this.state.task];
+    const initialEditStatus = this.state.completedTask[index].edit;
+    const newTasks = [...this.state.completedTask];
     newTasks[index].edit = !initialEditStatus;
-    this.setState({task: newTasks});
-    console.log("each list is clicked for edit" + this.state.task[index].edit + index);
+    this.setState({ completedTask: newTasks });
+    console.log("each list is clicked for edit" + this.state.completedTask[index].edit + index);
   }
 
   addTaskHandler = () => {
@@ -68,19 +79,19 @@ class TaskList extends React.Component<TaskListProperty> {
     };
 
     // copying the previous tasks
-    const newTasks = [...this.state.task];
+    const newTasks = [...this.state.completedTask];
     newTasks.push(aTask);
 
-    this.setState({task: newTasks});
+    this.setState({ completedTask: newTasks });
     console.log("task added. ");
   }
 
   handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>, index: number) => {
     const newTaskDescription = event.currentTarget.value;
     console.log(newTaskDescription);
-    const newTasks = [...this.state.task];
+    const newTasks = [...this.state.completedTask];
     newTasks[index].description = newTaskDescription;
-    this.setState({task: newTasks});
+    this.setState({ completedTask: newTasks });
   }
 
   handleSubmit = (event: React.FormEvent<HTMLFormElement>, index: number) => {
@@ -88,80 +99,96 @@ class TaskList extends React.Component<TaskListProperty> {
     this.handleEdit(index);
   };
 
-  handleRating = (newValue: number|null, index: number)=> {
-    const newTasks: TaskProperty[] = [...this.state.task];
+  handleRating = (newValue: number | null, index: number) => {
+    const newTasks: TaskProperty[] = [...this.state.completedTask];
     newTasks[index].rating = newValue;
-    
+
     // when a rating is given it sorts the tasks
     newTasks.sort((a, b) => {
-      if((a.rating != null) && (b.rating != null)){
+      if ((a.rating != null) && (b.rating != null)) {
         return b.rating - a.rating;
-      }else{
+      } else {
         return 0;
       }
     });
     console.log("index: " + index + " rating: " + newValue);
-    this.setState({task: newTasks});
+    this.setState({ completedTask: newTasks });
   }
- 
-  
-  render(){
+
+
+  render() {
     console.log("states are: ", this.state);
 
     const { classes } = this.props;
 
     // TextArea for adding tasks
-    const editText = this.state.task.map((value, index) => {
-      if(value.edit){
-        return (<MultilineTextFields handleChange={(event: React.ChangeEvent<HTMLTextAreaElement>)=>this.handleChange(event, index)} 
-        handleSubmit={(event: React.FormEvent<HTMLFormElement>) => this.handleSubmit(event,index)}/>)
+    const editText = this.state.completedTask.map((value, index) => {
+      if (value.edit) {
+        return (<MultilineTextFields handleChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => this.handleChange(event, index)}
+          handleSubmit={(event: React.FormEvent<HTMLFormElement>) => this.handleSubmit(event, index)} />)
       } else
-         return null;
+        return null;
     });
 
     return (
       <div>
-        <div style={{alignContent: "center", margin: "0.5%"}}>
-          <AddCircleIcon onClick={this.addTaskHandler}/>
-          <h3 style={{margin: "0px"}}>Add a task</h3>
+        <div style={{ alignContent: "center", margin: "0.5%" }}>
+          <AddCircleIcon onClick={this.addTaskHandler} />
+          <h3 style={{ margin: "0px" }}>Add a task</h3>
         </div>
-        {editText}
-        <List className={classes.root}>
-        {this.state.task.map((value, key) => {
-          const labelId = `checkbox-list-label-${value}`;
-          return (
-            <ListItem key={key} role={undefined} dense button>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked= {value.check}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                  onClick={this.handleCheck(key)}
-                />
-              </ListItemIcon>
-              <ListItemText disableTypography style={value.check?{textDecoration: "line-through"}: undefined} id={labelId} primary={value.description} />
-              <ListItemIcon itemID={key+"rating"}>
-                  <Rating name={"simple-controlled"+key} value={value.rating} max={3}
-                  onChange={(event: React.ChangeEvent<{}>, newValue:number|null)=> {
-                    console.log("rating key: " + key);
-                    this.handleRating( newValue, key)}
-                  } />
-              </ListItemIcon>
-              <ListItemSecondaryAction onClick={() => this.handleEdit(key)}>
-                <IconButton edge="end" aria-label="comments" >
-                  <CommentIcon />
-                </IconButton >
-              </ListItemSecondaryAction>
-            </ListItem>
-          );
-        })}
-      </List>
-      </div>      
+        <div style={{ display: "flex" }}>
+          {editText}
+          <List className={classes.root}>
+            {this.state.completedTask.map((value, key) => {
+              const labelId = `checkbox-list-label-${value}`;
+              return (
+                <ListItem key={key} role={undefined} dense button>
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={value.check}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                      onClick={this.handleCheck(key)}
+                    />
+                  </ListItemIcon>
+                  <ListItemText disableTypography style={value.check ? { textDecoration: "line-through" } : undefined} id={labelId} primary={value.description} />
+                  <ListItemIcon itemID={key + "rating"}>
+                    <Rating name={"simple-controlled" + key} value={value.rating} max={3}
+                      onChange={(event: React.ChangeEvent<{}>, newValue: number | null) => {
+                        console.log("rating key: " + key);
+                        this.handleRating(newValue, key)
+                      }
+                      } />
+                  </ListItemIcon>
+                  <ListItemSecondaryAction onClick={() => this.handleEdit(key)}>
+                    <IconButton edge="end" aria-label="comments" >
+                      <CommentIcon />
+                    </IconButton >
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
+          <List className={classes.root}>
+            {this.state.inCompletedTask.map((value, index) => {
+              const labelId = `checkbox-list-label-${value}`;
+              return (
+                <ListItem key={index} role={undefined} dense button>
+                  <ListItemText disableTypography style={value.check ? { textDecoration: "line-through" } : undefined}
+                    id={labelId} primary={value.description} />
+                  <p>{value.date?.toUTCString()}</p>
+                </ListItem>
+
+              );
+            })}
+          </List>
+        </div>
+      </div>
     );
   }
 }
 
 // "TaskList" is wrapped with "withStyles", so that "styles" is applied at this component
-export default withStyles(styles, {withTheme: true})(TaskList);
+export default withStyles(styles, { withTheme: true })(TaskList);
